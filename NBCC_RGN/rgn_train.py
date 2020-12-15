256b9844c3773882448d7f14e7b45650a561b8af
woosa7 <woosa7@gmail.com>
2020-12-15 11:22:10 +0900

    RGN model

--- NBCC_RGN/rgn_train.py
+++ NBCC_RGN/rgn_train.py
@@ -1,19 +1,24 @@
 import glob
+import numpy as np
+import collections
 import tensorflow as tf
-from tensorflow.keras import Sequential, layers, models
-from tensorflow.python.keras import Sequential
+from tensorflow.keras import layers, models
 
+# tf.compat.v1.disable_eager_execution()
 
-tf.random.set_seed(100)
+"""
+python 3.7
+tensorflow 2.3
+"""
 
-# print(tf.test.is_built_with_cuda())
-# print(tf.config.list_physical_devices('GPU'))
-
-phase = 'training'
-file_list = sorted(glob.glob('../RGN11/data/ProteinNet11/{}/*'.format(phase)))
+NUM_DIHEDRALS = 3
+NUM_DIMENSIONS = 3
 
 batch_size = 32
+tf.random.set_seed(100)
 
+phase = 'training'
+file_list = sorted(glob.glob('./RGN11/data/ProteinNet11/{}/*'.format(phase)))
 dataset = tf.data.TFRecordDataset(tf.data.Dataset.list_files(file_list)).batch(batch_size).shuffle(buffer_size=32)
 
 def get_data():
@@ -60,49 +65,195 @@ def get_data():
     return ids, inputs, ter_y
 
 
-def get_model_old(num_steps):
-    model = Sequential()
+def reduce_mean_angle(weights, angles):
+    """
+        weights: [BATCH_SIZE, NUM_ANGLES]
+        angles:  [NUM_ANGLES, NUM_DIHEDRALS]
+        Returns: [BATCH_SIZE, NUM_DIHEDRALS]
+    """
+    weights = tf.convert_to_tensor(weights, name='weights')
+    angles  = tf.convert_to_tensor(angles,  name='angles')
 
-    # Bi-directional LSTM
-    model.add(layers.Bidirectional(
-        layers.LSTM(800, return_sequences=True, dropout=0.5), input_shape=(num_steps, 62), name='bi_lstm'
-    ))
-    model.add(layers.Bidirectional(
-        layers.LSTM(800, return_sequences=True, dropout=0.5), name='bi_lstm_2'
-    ))
+    # use real-numbered pairs of values
+    sins = tf.sin(angles)
+    coss = tf.cos(angles)
 
-    # TODO : --> dihedrals (alphabet) --> coordinates
-    # model.add(layers.Dense(60, activation='relu'))
-    # model.add(layers.BatchNormalization())
-    # model.add(layers.Dense(3))
-    # model.add(layers.Softmax())
+    y_coords = tf.matmul(weights, sins)
+    x_coords = tf.matmul(weights, coss)
 
-    model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
-    model.summary()
+    return tf.atan2(y_coords, x_coords)
 
-    return model
+
+def dihedral_to_point(dihedral):
+    """ Takes triplets of dihedral angles (omega, phi, psi) and returns 3D points ready for use in reconstruction of coordinates.
+
+        dihedral: [NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+        Returns:  [NUM_STEPS x NUM_DIHEDRALS, BATCH_SIZE, NUM_DIMENSIONS]
+    """
+    # Bond lengths and angles are based on idealized averages.
+    r     = np.array([145.801, 152.326, 132.868], dtype='float32')      # BOND_LENGTHS
+    theta = np.array([  2.124,   1.941,   2.028], dtype='float32')      # BOND_ANGLES
+
+    dihedral = tf.convert_to_tensor(dihedral) # [NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+
+    num_steps  = tf.shape(dihedral)[0]
+    batch_size = dihedral.get_shape().as_list()[1] # important to use get_shape() to keep batch_size fixed for performance reasons
+
+    r_cos_theta = tf.constant(r * np.cos(np.pi - theta), name='r_cos_theta') # [NUM_DIHEDRALS]
+    r_sin_theta = tf.constant(r * np.sin(np.pi - theta), name='r_sin_theta') # [NUM_DIHEDRALS]
+
+    pt_x = tf.tile(tf.reshape(r_cos_theta, [1, 1, -1]), [num_steps, batch_size, 1], name='pt_x') # [NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+    pt_y = tf.multiply(tf.cos(dihedral), r_sin_theta,                               name='pt_y') # [NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+    pt_z = tf.multiply(tf.sin(dihedral), r_sin_theta,                               name='pt_z') # [NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+
+    pt = tf.stack([pt_x, pt_y, pt_z])                                                       # [NUM_DIMS, NUM_STEPS, BATCH_SIZE, NUM_DIHEDRALS]
+    pt_perm = tf.transpose(pt, perm=[1, 3, 2, 0])                                           # [NUM_STEPS, NUM_DIHEDRALS, BATCH_SIZE, NUM_DIMS]
+    pt_final = tf.reshape(pt_perm, [num_steps * NUM_DIHEDRALS, batch_size, NUM_DIMENSIONS]) # [NUM_STEPS x NUM_DIHEDRALS, BATCH_SIZE, NUM_DIMS]
+
+    return pt_final
+
+
+@tf.function
+def point_to_coordinate(pt, num_fragments=6, name=None):
+    """ Takes points from dihedral_to_point and sequentially converts them into the coordinates of a 3D structure.
+        Reconstruction is done in parallel, by independently reconstructing num_fragments fragments and then
+        reconstituting the chain at the end in reverse order. The core reconstruction algorithm is NeRF, based on
+        DOI: 10.1002/jcc.20237 by Parsons et al. 2005. The parallelized pNERF version is described in
+        DOI: 10.1002/jcc.25772 by AlQuraishi 2019.
+    """
+
+    with tf.name_scope(name='point_to_coordinate') as scope:
+        pt = tf.convert_to_tensor(pt, name='pt')
+        print('pt 1 :', pt)       # (None, 32, 3)
+
+        s = tf.shape(pt)[0]     # NUM_STEPS x NUM_DIHEDRALS
+
+        # initial three coordinates (specifically chosen to eliminate need for extraneous matmul)
+        Triplet = collections.namedtuple('Triplet', 'a, b, c')
+
+        batch_size = pt.get_shape().as_list()[1] # BATCH_SIZE
+        init_mat = np.array([[-np.sqrt(1.0 / 2.0), np.sqrt(3.0 / 2.0), 0], [-np.sqrt(2.0), 0, 0], [0, 0, 0]], dtype='float32')
+        init_coords = Triplet(*[tf.reshape(tf.tile(row[np.newaxis], tf.stack([num_fragments * batch_size, 1])),
+                                           [num_fragments, batch_size, NUM_DIMENSIONS]) for row in init_mat])
+                                           # 3 * [6, 32, 3]
+                                           # NUM_DIHEDRALS x [NUM_FRAGS, BATCH_SIZE, NUM_DIMENSIONS]
+
+        # pad points to yield equal-sized fragments
+        r = ((num_fragments - (s % num_fragments)) % num_fragments)          # (NUM_FRAGS x FRAG_SIZE) - (NUM_STEPS x NUM_DIHEDRALS)
+        pt = tf.pad(pt, [[0, r], [0, 0], [0, 0]])                            # [NUM_FRAGS x FRAG_SIZE, BATCH_SIZE, NUM_DIMENSIONS]
+        pt = tf.reshape(pt, [num_fragments, -1, batch_size, NUM_DIMENSIONS]) # [NUM_FRAGS, FRAG_SIZE,  BATCH_SIZE, NUM_DIMENSIONS]
+        pt = tf.transpose(pt, perm=[1, 0, 2, 3])                             # [FRAG_SIZE, NUM_FRAGS,  BATCH_SIZE, NUM_DIMENSIONS]
+
+        print('pt 2 :', pt)     # (None, 6, 32, 3)
+        print()
+
+        # extension function used for single atom reconstruction and whole fragment alignment
+        def extend(tri, pt, multi_m):
+            """
+            Args:
+                tri: NUM_DIHEDRALS x [NUM_FRAGS/0,         BATCH_SIZE, NUM_DIMENSIONS]
+                pt:                  [NUM_FRAGS/FRAG_SIZE, BATCH_SIZE, NUM_DIMENSIONS]
+                multi_m: bool indicating whether m (and tri) is higher rank. pt is always higher rank; what changes is what the first rank is.
+            """
+            bc = tf.nn.l2_normalize(tri.c - tri.b, -1, name='bc')                                        # [NUM_FRAGS/0, BATCH_SIZE, NUM_DIMS]
+            n = tf.nn.l2_normalize(tf.linalg.cross(tri.b - tri.a, bc), -1, name='n')                            # [NUM_FRAGS/0, BATCH_SIZE, NUM_DIMS]
+            if multi_m: # multiple fragments, one atom at a time.
+                m = tf.transpose(tf.stack([bc, tf.linalg.cross(n, bc), n]), perm=[1, 2, 3, 0], name='m')        # [NUM_FRAGS,   BATCH_SIZE, NUM_DIMS, 3 TRANS]
+            else: # single fragment, reconstructed entirely at once.
+                s = tf.pad(tf.shape(pt), [[0, 1]], constant_values=3)                                    # FRAG_SIZE, BATCH_SIZE, NUM_DIMS, 3 TRANS
+                m = tf.transpose(tf.stack([bc, tf.linalg.cross(n, bc), n]), perm=[1, 2, 0])                     # [BATCH_SIZE, NUM_DIMS, 3 TRANS]
+                m = tf.reshape(tf.tile(m, [s[0], 1, 1]), s, name='m')                                    # [FRAG_SIZE, BATCH_SIZE, NUM_DIMS, 3 TRANS]
+            coord = tf.add(tf.squeeze(tf.matmul(m, tf.expand_dims(pt, 3)), axis=3), tri.c, name='coord') # [NUM_FRAGS/FRAG_SIZE, BATCH_SIZE, NUM_DIMS]
+            return coord
+
+        # loop over FRAG_SIZE in NUM_FRAGS parallel fragments, sequentially generating the coordinates for each fragment across all batches
+        i = tf.constant(0)
+        s_padded = tf.shape(pt)[0]
+        print('s_padded :', s_padded)
+
+        coords_ta = tf.TensorArray(tf.float32, size=s_padded)
+        print('coords_ta :', coords_ta)
+        print()
+
+        def loop_extend(i, tri, coords_ta): # FRAG_SIZE x [NUM_FRAGS, BATCH_SIZE, NUM_DIMENSIONS]
+            coord = extend(tri, pt[i], True)
+            return [i + 1, Triplet(tri.b, tri.c, coord), coords_ta.write(i, coord)]
+
+
+        _, tris, coords_pretrans_ta = tf.while_loop(lambda i, _1, _2: i < s_padded, loop_extend, [i, init_coords, coords_ta])
+
+        # cond, body, loop_vars
+        # cond = lambda i, _1, _2: i < 0
+        # body = lambda i, _1, _2: loop_extend
+        # _, tris, coords_pretrans_ta = tf.while_loop(cond, body, [i, init_coords, coords_ta])
+                                      # NUM_DIHEDRALS x [NUM_FRAGS, BATCH_SIZE, NUM_DIMENSIONS],
+                                      # FRAG_SIZE x [NUM_FRAGS, BATCH_SIZE, NUM_DIMENSIONS]
+
+        print('coords_pretrans_ta :', coords_pretrans_ta)
+
+        # loop over NUM_FRAGS in reverse order, bringing all the downstream fragments in alignment with current fragment
+        coords_pretrans = tf.transpose(coords_pretrans_ta.stack(), perm=[1, 0, 2, 3]) # [NUM_FRAGS, FRAG_SIZE, BATCH_SIZE, NUM_DIMENSIONS]
+        print('coords_pretrans :', coords_pretrans)         # (6, None, 32, 3)
+
+        i = tf.shape(coords_pretrans)[0] # NUM_FRAGS
+
+        def loop_trans(i, coords):
+            transformed_coords = extend(Triplet(*[di[i] for di in tris]), coords, False)
+            return [i - 1, tf.concat([coords_pretrans[i], transformed_coords], 0)]
+
+        _, coords_trans = tf.while_loop(lambda i, _: i > -1, loop_trans, [i - 2, coords_pretrans[-1]])
+                          # [NUM_FRAGS x FRAG_SIZE, BATCH_SIZE, NUM_DIMENSIONS]
+
+        print('coords_trans :', coords_trans)       # (None, 32, 3)
+
+        # lose last atom and pad from the front to gain an atom ([0,0,0], consistent with init_mat), to maintain correct atom ordering
+        coords = tf.pad(coords_trans[:s-1], [[1, 0], [0, 0], [0, 0]]) # [NUM_STEPS x NUM_DIHEDRALS, BATCH_SIZE, NUM_DIMENSIONS]
+
+        print('coords :', coords)   # (None, 32, 3)
+
+        return coords
+
 
 def get_model(batch_size, alphabet):
+    # (N, 32, 62)
     input_x = layers.Input(shape=(batch_size, 62))
 
+    # -------------------------------------------------
     # Bi-directional LSTM
     out_x = layers.Bidirectional(layers.LSTM(800, return_sequences=True, dropout=0.5), name='bi_lstm1')(input_x)
     out_x = layers.Bidirectional(layers.LSTM(800, return_sequences=True, dropout=0.5), name='bi_lstm2')(out_x)
+    # RNN output : (N, 32, 1600)
 
+    # -------------------------------------------------
     # dihedrals
-    out_x = layers.Dense(60, name='flatten')(out_x)
-    flattened_out = tf.reshape(out_x, [-1, 60])
-    out_x = layers.Softmax(name='softmax')(flattened_out)
+    out_x = layers.Dense(60, name='flatten')(out_x)             # (N, 32, 60)
+    num_steps = tf.shape(out_x)[0]
 
-    sine = tf.matmul(out_x, tf.math.sin(alphabet))
-    cosine = tf.matmul(out_x, tf.math.cos(alphabet))
-    out_x = tf.math.atan2(sine, cosine)
+    flat_out_x = tf.reshape(out_x, [-1, 60])
+    flat_out_x = layers.Softmax(name='softmax')(flat_out_x)     # (N, 60)
+    print('softmax', flat_out_x.shape)
+    print('alphabet', alphabet.shape)
+
+    flat_dihedrals = reduce_mean_angle(flat_out_x, alphabet)     # (N, 60) * (60, 3) = (N, 3)
+    print('flat_dihedrals', flat_dihedrals.shape)
+
+    dihedrals = tf.reshape(flat_dihedrals, [num_steps, batch_size, 3])  # (None, 32, 3)
+    print('dihedrals', dihedrals.shape)
+
+    # -------------------------------------------------
+    # _coordinates
+    points = dihedral_to_point(dihedrals)       # (None, 32, 3)
+    print('points', points.shape)
+
+    coordinates = point_to_coordinate(points)
+    # print('coordinates', coordinates.shape)
 
 
-    model = models.Model(input_x, out_x)
+    # TODO
+    model = models.Model(input_x, dihedrals)
 
-    model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'])
-    model.summary()
+    model.compile(optimizer='adam', loss='mse', metrics=['mean_squared_error'], experimental_run_tf_function=False)
+    # model.summary()
 
     return model
 
@@ -113,11 +264,10 @@ alphabet = tf.random.uniform(shape=[60,3], minval=-3.14, maxval=3.14)
 # create model
 model = get_model(batch_size, alphabet)
 
-
-# # p_list = []
-# for k in range(300):
+# p_list = []
+# for k in range(3000):
 #     id_list, X, y = get_data()
-#     print(X.shape, y.shape)
+#     print(k+1, X.shape, y.shape)
 #
 #     hist = model.fit(X, y)
 
@@ -132,4 +282,4 @@ model = get_model(batch_size, alphabet)
     # -------------------------------
 
 
-
+# -------------------------------------------------------------
