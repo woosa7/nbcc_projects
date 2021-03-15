c9d8f61d58b41283a9cd605fb271c5cd3cf92b8f
woosa7 <woosa7@gmail.com>
2021-03-15 17:21:28 +0900

    set residue info

---
+++ analyze_protein_trajectory/HP36/traj_A/set_residue_info/run.sh
@@ -0,0 +1,5 @@
+cp ../../set_pql_param/protein.pql .
+
+python run_set_residue_info.py
+
+rm -rf protein.pql
---
+++ analyze_protein_trajectory/HP36/traj_A/set_residue_info/run_set_residue_info.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+"""
+residue information to be used in the decomposition calculation
+
+input file  : protein.pql
+output file : residue_info.dat
+"""
+
+atom,atom_no,atom_name,res_name,res_no,coord_x,coord_y,coord_z = np.loadtxt('protein.pql',
+            skiprows=1,
+            dtype=[('a',"|S4"),('b',int),('c',"|S4"),('d',"|S4"),('e',int),('f',float),('g',float),('h',float)],
+            unpack=True)
+
+n_atom = len(atom_no)
+n_res = res_no[-1]
+print('total number of atoms = %s' % n_atom)
+print('total number of residues = %s' % n_res)
+
+dict = {}
+for n in res_no:
+    if n in dict:
+        dict[n] += 1
+    else:
+        dict[n] = 1
+
+f = open('residue_info.dat', "w")
+print('{:>4d}'.format(n_res), file=f)
+
+for i in range(n_res):
+    rno = i+1
+    if i == 0:
+        istart = 1
+        iend = dict[rno]
+    else:
+        istart = iend + 1
+        iend = iend + dict[rno]
+
+    print('{:>4d} {:>5d} {:>5d}'.format(rno, istart, iend), file=f)
+
+f.close()
+
+# =============================================================================
